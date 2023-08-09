e59988b68ad7e0b6d986fe7b057706c5af1895c3
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-08-09 15:02:39 +0300

    added a dogs  route

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_191324) do
+ActiveRecord::Schema.define(version: 2023_08_09_115312) do
 
   create_table "cheeses", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.boolean "is_best_seller"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 puts '🧀 Seeding cheeses...'
 
-Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
-Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
-Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
+# Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
+# Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
+# Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
+
+Dog.create!([{name:"jay", breed: "beagle", age:2}, {name:"jayck", breed: "gle", age:3}, {name:"jase", breed: "begle", age:5}])
 
 puts '✅ Done seeding'
